623d99d3216c952623251a12fe9dd0d600d65a6b
fmsrodrigues <filipe_msr@hotmail.com>
2023-01-19 20:22:07 -0300

    nlw-setup 03 mobile

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { ScrollView, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
@@ -11,6 +12,8 @@ const minimumSummaryDatesSize = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSize - dateFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -35,6 +38,7 @@ export function Home() {
           {dateFromYearStart.map(date => (
             <HabitDay 
               key={date.toISOString()}
+              onPress={() => { navigate('habit', { date: date.toISOString() }); }}
             />
           ))}
           {
