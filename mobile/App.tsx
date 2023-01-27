9c1b08c68db0bfa58e358108102177140c63b4f8
fmsrodrigues <filipe_msr@hotmail.com>
2023-01-27 15:07:59 -0300

    nlw-setup push notifications mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,10 +2,19 @@ import "./src/lib/dayjs";
 
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from "@expo-google-fonts/inter";
+import * as Notifications from 'expo-notifications';
 
 import { Routes } from "./src/routes";
 
 import { Loading } from './src/components/Loading';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false
+  })
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -15,12 +24,30 @@ export default function App() {
     Inter_800ExtraBold
   });
 
+  async function scheduleNotification() {
+    const trigger = new Date(Date.now());
+    trigger.setMinutes(trigger.getMinutes() + 1);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Habits ❤️",
+        body: "Você praticou seus hábitos hoje?"
+      },
+      trigger
+    })
+  }
+
+  async function getScheduledNotifications() {
+    const schedule = await Notifications.getAllScheduledNotificationsAsync();
+    console.log(schedule);
+  }
+
   if(!fontsLoaded) return <Loading />;
 
   return (
     <>
       <Routes />
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
     </>
   );
 }
